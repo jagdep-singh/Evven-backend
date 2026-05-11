f64fda58f9341cf78d6afd38074e06d910fa3a6f
jagdep-singh <jagdeep.singh12a@gmail.com>
2026-05-11 11:04:13 +0530

    made schemas and services for user auth

--- models/user.py
+++ models/user.py
@@ -1,20 +1,27 @@
-from sqlalchemy import Column, String, TIMESTAMP
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, String, TIMESTAMP , Boolean , Enum as SQLEnum 
+from sqlalchemy.dialects.postgresql import UUID 
 from sqlalchemy.sql import func
 
 from database import Base
+from enum import Enum
 
 import uuid
 
+
+class AuthProvider(Enum):
+    LOCAL = 'local'
+    GOOGLE = 'google'
+    
 class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    
     name = Column(String, nullable=False)
-    
     email = Column(String, unique=True, nullable=False)
-    
-    password_hash = Column(String, nullable=False)
-    
+    password_hash = Column(String, nullable=True)
+    google_id = Column(String, unique=True, nullable=True)
+    auth_provider = Column(SQLEnum(AuthProvider), nullable=False, default=AuthProvider.LOCAL) 
+    profile_picture = Column(String, nullable=True)
+    is_active = Column(Boolean, nullable=False, default=True)
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
---
+++ schemas/user.py
@@ -0,0 +1,40 @@
+from datetime import datetime as dt
+from uuid import UUID
+
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    name : str
+    email: EmailStr
+    password: str
+    
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+    
+class GoogleAuthRequest(BaseModel):
+    token: str
+    
+class TokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+    
+    
+class UserResponse(BaseModel):
+    id: UUID
+    name: str
+    email: EmailStr
+    auth_provider: str
+    profile_picture: str | None = None
+    created_at: dt
+    
+    model_config = {
+        "from_attributes": True
+    }
+    
+class UserUpdate(BaseModel):
+    name: str | None = None
+    profile_picture: str | None = None
+    
+    
