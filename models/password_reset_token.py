765494cc62b21324881725f5f1a4b844fe3190d6
rohit <rohit19801496@@gmail.com>
2026-06-07 12:14:49 +0530

    added relations in model and changed table name, there is no error on fastapi dev run

--- models/password_reset_token.py
+++ models/password_reset_token.py
@@ -2,12 +2,13 @@ import uuid
 
 from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, String  # type: ignore
 from sqlalchemy.dialects.postgresql import UUID  # type: ignore
+from sqlalchemy.orm import relationship
 
 from core.database import Base
 
 
 class PasswordResetToken(Base):
-    __tablename__ = "password_reset_token"
+    __tablename__ = "password-reset-table"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
@@ -16,3 +17,4 @@ class PasswordResetToken(Base):
     used = Column(Boolean, default=False, nullable=False)
 
     # relationships
+    user = relationship("User", back_populates="password_reset_tokens")
